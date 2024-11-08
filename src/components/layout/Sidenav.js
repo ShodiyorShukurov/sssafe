49fdca7afbc05bc800fe9d07a213af55818b27b8
ShodiyorShukurov <shodiyorshukurov07@gmail.com>
2024-11-08 16:58:31 +0500

    Edit icon

--- src/components/layout/Sidenav.js
+++ src/components/layout/Sidenav.js
@@ -4,19 +4,8 @@ import logo from "../../assets/images/logo.png";
 import { data } from "../../mock/data";
 
 function Sidenav() {
-   const { pathname } = useLocation();
-   const page = pathname.replace("/", "");
-  const user = [
-    <svg
-      width="20"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      fill="currentColor"
-      class="size-4"
-    >
-      <path d="M8 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM3.156 11.763c.16-.629.44-1.21.813-1.72a2.5 2.5 0 0 0-2.725 1.377c-.136.287.102.58.418.58h1.449c.01-.077.025-.156.045-.237ZM12.847 11.763c.02.08.036.16.046.237h1.446c.316 0 .554-.293.417-.579a2.5 2.5 0 0 0-2.722-1.378c.374.51.653 1.09.813 1.72ZM14 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM3.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 13c-.552 0-1.013-.455-.876-.99a4.002 4.002 0 0 1 7.753 0c.136.535-.324.99-.877.99H5Z" />
-    </svg>,
-  ];
+  const { pathname } = useLocation();
+  const page = pathname.replace("/", "");
 
   return (
     <>
@@ -26,17 +15,24 @@ function Sidenav() {
       </div>
       <hr />
       <Menu theme="light" mode="inline">
-        <Menu.Item key="1">
+        <Menu.Item key="2">
           <NavLink to="/user-list">
             <span
               className="icon"
               style={{
-                background: page
+                background: page === "user-list" ,
               }}
             >
-              {user}
+              <svg
+                width="20"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+              >
+                <path d="M8 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM3.156 11.763c.16-.629.44-1.21.813-1.72a2.5 2.5 0 0 0-2.725 1.377c-.136.287.102.58.418.58h1.449c.01-.077.025-.156.045-.237ZM12.847 11.763c.02.08.036.16.046.237h1.446c.316 0 .554-.293.417-.579a2.5 2.5 0 0 0-2.722-1.378c.374.51.653 1.09.813 1.72ZM14 7.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0ZM3.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3ZM5 13c-.552 0-1.013-.455-.876-.99a4.002 4.002 0 0 1 7.753 0c.136.535-.324.99-.877.99H5Z" />
+              </svg>
             </span>
-            <span className="label">{data.sideNav.pathName}</span>
+            <span className="label">Users List</span>
           </NavLink>
         </Menu.Item>
       </Menu>
